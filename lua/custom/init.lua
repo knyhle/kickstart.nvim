3319a25bfada27da87f4a0ed5f7bb3589cb70d93
Kenny Le <kenny.le@gm.com>
2023-07-06 12:53:17 -0500

    add split keymappings

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,3 +1,5 @@
 
 vim.cmd([[ autocmd VimEnter * Copilot auth ]])
+vim.keymap.set("n", "<leader>vs", "<cmd> vsplit <CR>", { desc = "Vertical split" })
+vim.keymap.set("n", "<leader>vh", "<cmd> split <CR>", { desc = "Horizontal split" })
 
