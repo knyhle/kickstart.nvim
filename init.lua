478a25bc47b67cde18f24941f5cc3f22464061c6
Kenny Le <kenny.le@gm.com>
2023-07-07 13:32:46 -0500

    remove tmux and onedark from status line

--- init.lua
+++ init.lua
@@ -149,7 +149,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        -- theme = 'onedark',
         component_separators = '|',
         section_separators = '',
       },
