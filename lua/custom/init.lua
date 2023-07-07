fc02c3f4e08c7cf5120d37b09aac2369ede8c361
Kenny Le <kennyle1412@gmail.com>
2023-07-07 18:36:52 -0500

    add back tmux navigation with tmuxlua

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,21 +1,12 @@
 
 vim.cmd.colorscheme 'catppuccin'
+vim.cmd([[ autocmd VimEnter * Copilot auth ]])
 
 vim.opt.splitright = true
+vim.opt.swapfile = false
 
-vim.cmd([[ autocmd VimEnter * Copilot auth ]])
 vim.keymap.set("n", "<leader>vs", "<cmd> vsplit <CR>", { desc = "Vertical split" })
 vim.keymap.set("n", "<leader>vh", "<cmd> split <CR>", { desc = "Horizontal split" })
-
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
-
--- vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft <CR>", { desc = "Move to left window" })
--- vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown <CR>", { desc = "Move to bottom window" })
--- vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp <CR>", { desc = "Move to top window" })
--- vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight <CR>", { desc = "Move to right window" })
 
 vim.keymap.set('n', '<C-p>', require('telescope.builtin').git_files, { desc = 'Search [G]it [F]iles' })
 
