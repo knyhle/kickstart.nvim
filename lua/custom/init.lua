ce06ae204cfc583cf10d6ac2d17a845626f73318
Kenny Le <kenny.le@gm.com>
2023-07-07 11:37:56 -0500

    set vsplit right

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -1,4 +1,6 @@
 
+vim.opt.splitright = true
+
 vim.cmd([[ autocmd VimEnter * Copilot auth ]])
 vim.keymap.set("n", "<leader>vs", "<cmd> vsplit <CR>", { desc = "Vertical split" })
 vim.keymap.set("n", "<leader>vh", "<cmd> split <CR>", { desc = "Horizontal split" })
