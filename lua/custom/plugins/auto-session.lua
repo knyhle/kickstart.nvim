05ab1606bfe897b2e21346cb18fc8a979b5db1d2
Kenny Le <kennyle1412@gmail.com>
2024-02-15 06:25:02 +0000

    feat: Switch session management from auto-session to persistence
    This commit switches the session management plugin from 'rmagatti/auto-session' to 'folke/persistence.nvim'. The change includes enabling the persistence plugin and disabling the auto-session plugin. The persistence plugin is configured to restore sessions with specific key bindings.

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,12 +1,12 @@
 return {
-    {
-      'rmagatti/auto-session',
-      config = function()
-        require("auto-session").setup {
-          log_level = "error",
-          auto_session_suppress_dirs = { "~", "~/repos" },
-        }
-      end
-    }
-  }
-  
+  -- {
+  --   'rmagatti/auto-session',
+  --   config = function()
+  --     require("auto-session").setup {
+  --       log_level = "error",
+  --       auto_session_suppress_dirs = { "~", "~/repos" },
+  --     }
+  --   end
+  -- }
+}
+
