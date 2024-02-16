407909584cdf83935219e61699cefe241d40e1ab
Kenny Le <kennyle1412@gmail.com>
2024-02-16 13:03:16 -0600

    update treesitter movements

--- init.lua
+++ init.lua
@@ -422,7 +422,32 @@ vim.defer_fn(function()
           ['if'] = '@function.inner',
           ['ac'] = '@class.outer',
           ['ic'] = '@class.inner',
+          ['ad'] = '@conditional.outer',
+          ['id'] = '@conditional.inner',
+          ['as'] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
         },
+        -- You can choose the select mode (default is charwise 'v')
+        --
+        -- Can also be a function which gets passed a table with the keys
+        -- * query_string: eg '@function.inner'
+        -- * method: eg 'v' or 'o'
+        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+        -- mapping query_strings to modes.
+        selection_modes = {
+          ['@parameter.outer'] = 'v', -- charwise
+          ['@function.outer'] = 'V',  -- linewise
+          ['@class.outer'] = '<c-v>', -- blockwise
+        },
+        -- If you set this to `true` (default is `false`) then any textobject is
+        -- extended to include preceding or succeeding whitespace. Succeeding
+        -- whitespace has priority in order to act similarly to eg the built-in
+        -- `ap`.
+        --
+        -- Can also be a function which gets passed a table with the keys
+        -- * query_string: eg '@function.inner'
+        -- * selection_mode: eg 'v'
+        -- and should return true of false
+        include_surrounding_whitespace = false,
       },
       move = {
         enable = true,
@@ -430,18 +455,33 @@ vim.defer_fn(function()
         goto_next_start = {
           [']m'] = '@function.outer',
           [']]'] = '@class.outer',
+          --
+          -- You can use regex matching (i.e. lua pattern) and/or pass a list in a "query" key to group multiple queires.
+          ["]l"] = "@loop.outer",
+          ["]d"] = "@conditional.outer",
+
+          -- You can pass a query group to use query from `queries/<lang>/<query_group>.scm file in your runtime path.
+          -- Below example nvim-treesitter's `locals.scm` and `folds.scm`. They also provide highlights.scm and indent.scm.
+          ["]s"] = { query = "@scope", query_group = "locals", desc = "Next scope" },
+          ["]z"] = { query = "@fold", query_group = "folds", desc = "Next fold" },
         },
         goto_next_end = {
           [']M'] = '@function.outer',
           [']['] = '@class.outer',
+          ["]L"] = "@loop.outer",
+          ["]D"] = "@conditional.outer",
         },
         goto_previous_start = {
           ['[m'] = '@function.outer',
           ['[['] = '@class.outer',
+          ["[l"] = "@loop.outer",
+          ["[d"] = "@conditional.outer",
         },
         goto_previous_end = {
           ['[M'] = '@function.outer',
           ['[]'] = '@class.outer',
+          ["[L"] = "@loop.inner",
+          ["[D"] = "@conditional.outer",
         },
       },
       -- swap = {
